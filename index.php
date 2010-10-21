cdd206db5b8bcf1ca6885315ed613c82e649041d
Bela Terjek <bela@netc.hu>
2010-10-21 21:28:39 +0200

    Index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Test grid</title>
+
+    <!-- ** CSS ** -->
+    <!-- base library -->
+    <link rel="stylesheet" type="text/css" href="/common/js/ext/resources/css/ext-all.css" />
+
+    <!-- ** Javascript ** -->
+    <!-- ExtJS library: base/adapter -->
+    <script type="text/javascript" src="/common/js/ext/adapter/ext/ext-base.js"></script>
+
+    <!-- ExtJS library: all widgets -->
+    <script type="text/javascript" src="/common/js/ext/ext-all-debug.js"></script>
+
+    <!-- overrides to base library -->
+
+    <!-- page specific -->
+    <script type="text/javascript" src="array-grid.js"></script>
+
+</head>
+
+<body>
+</body>
+</html>
